c696f8ca68ea66e2407a2ab29d14587ab1109709
A2N2 <129242580+atoonatoo@users.noreply.github.com>
2024-09-04 20:27:58 +0900

    commit

--- lodgingCommander/src/main/java/com/hotel/lodgingCommander/contoller/UserController.java
+++ lodgingCommander/src/main/java/com/hotel/lodgingCommander/contoller/UserController.java
@@ -17,48 +17,48 @@ import java.util.Optional;
 @RequestMapping("/user/api")
 public class UserController {
 
-    private final UserService service;
-    private final UserRepository repository;
-
-    @PostMapping("/join")
-    public ResponseEntity<Boolean> join(@RequestBody UserModel userModel) {
-        return ResponseEntity.ok(service.join(userModel));
-    }
-
-    @PostMapping("/logIn")
-    public ResponseEntity<Boolean> logIn(@RequestBody UserModel userModel) {
-        return ResponseEntity.ok(service.logIn(userModel));
-    }
-
-
-    @GetMapping("/findById")
-    public ResponseEntity<Optional<User>> findById() {
-        return ResponseEntity.ok(repository.findBy());
-    }
-
-    @PutMapping("/update")
-    public ResponseEntity<Boolean> update(@RequestBody UserModel userModel) {
-        return service.update(userModel);
-    }
-
-    @DeleteMapping("/delete")
-    public ResponseEntity<Boolean> delete(@RequestBody UserModel userModel) {
-        return service.delete(userModel);
-    }
-
-    @GetMapping("/list")
-    public ResponseEntity<List<User>> findAll() {
-        return ResponseEntity.ok(repository.findAll());
-    }
-
-    @GetMapping("/{email}")
-    public ResponseEntity<Boolean> existsByEmail (String email){
-        return ResponseEntity.ok(repository.existsByEmail(email));
-    }
-
-    @GetMapping("/count")
-    public ResponseEntity<Long> count () {
-        return ResponseEntity.ok(repository.count());
-    }
+//    private final UserService service;
+//    private final UserRepository repository;
+//
+//    @PostMapping("/join")
+//    public ResponseEntity<Boolean> join(@RequestBody UserModel userModel) {
+//        return ResponseEntity.ok(service.join(userModel));
+//    }
+//
+//    @PostMapping("/logIn")
+//    public ResponseEntity<Boolean> logIn(@RequestBody UserModel userModel) {
+//        return ResponseEntity.ok(service.logIn(userModel));
+//    }
+//
+//
+//    @GetMapping("/findById")
+//    public ResponseEntity<Optional<User>> findById() {
+//        return ResponseEntity.ok(repository.findBy());
+//    }
+//
+//    @PutMapping("/update")
+//    public ResponseEntity<Boolean> update(@RequestBody UserModel userModel) {
+//        return service.update(userModel);
+//    }
+//
+//    @DeleteMapping("/delete")
+//    public ResponseEntity<Boolean> delete(@RequestBody UserModel userModel) {
+//        return service.delete(userModel);
+//    }
+//
+//    @GetMapping("/list")
+//    public ResponseEntity<List<User>> findAll() {
+//        return ResponseEntity.ok(repository.findAll());
+//    }
+//
+//    @GetMapping("/{email}")
+//    public ResponseEntity<Boolean> existsByEmail (String email){
+//        return ResponseEntity.ok(repository.existsByEmail(email));
+//    }
+//
+//    @GetMapping("/count")
+//    public ResponseEntity<Long> count () {
+//        return ResponseEntity.ok(repository.count());
+//    }
 
 }
