c2b6079febe2c429d6dde2f4de7dc359e65cd8e1
A2N2 <129242580+atoonatoo@users.noreply.github.com>
2024-09-05 04:00:46 +0900

    commit

--- lodgingCommander/src/main/java/com/hotel/lodgingCommander/config/WebConfig.java
+++ lodgingCommander/src/main/java/com/hotel/lodgingCommander/config/WebConfig.java
@@ -2,9 +2,10 @@ package com.hotel.lodgingCommander.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해 CORS 설정을 적용합니다
